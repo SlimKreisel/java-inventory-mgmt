38c63d0dae905d19a3b041336bc78743d6a3905f
Khalid <C23727729@mytudublin.ie>
2025-09-25 23:28:12 +0100

    layout done, Nearly finish with JDBC

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -7,8 +7,8 @@ public class DatabaseConnection {
 
         private static final String URL =
                 "jdbc:mysql://127.0.0.1:3306/?user=root";
-        private static final String USER = "root";      // <-- your MySQL username
-        private static final String PASS = "Khalidneymar11";  // <-- your MySQL password
+        private static final String USER = "root";
+        private static final String PASS = "Khalidneymar11";
 
         public static Connection getConnection() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASS);
