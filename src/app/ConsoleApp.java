8ffc7d26511e5436e435e6b86038ec2e98a1307b
Khalid <C23727729@mytudublin.ie>
2025-09-26 18:33:31 +0100

    features: add DB schema + JDBC DAO + console App

--- src/app/ConsoleApp.java
+++ src/app/ConsoleApp.java
@@ -1,4 +1,32 @@
 package app;
 
+
+
+import dao.ProductDAO;
+import model.Product;
+
 public class ConsoleApp {
+    public static void main(String[] args) {
+        ProductDAO dao = new ProductDAO();
+
+        System.out.println("Initial data:");
+        System.out.println(dao.getAll());
+
+        System.out.println("\nAdding: USB-C Cable...");
+        dao.add(new Product("USB-C Cable", 20, 7.99));
+        System.out.println(dao.getAll());
+
+        System.out.println("\nUpdate quantity of id=1 to 9...");
+        dao.updateQuantity(1, 9);
+        System.out.println(dao.getAll());
+
+        System.out.println("\nUpdate price of id=2 to 24.99...");
+        dao.updatePrice(2, 24.99);
+        System.out.println(dao.getAll());
+
+        System.out.println("\nTry delete id=999 (likely no-op)...");
+        dao.delete(999);
+        System.out.println(dao.getAll());
+    }
 }
+
--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
         private static final String URL =
-                "jdbc:mysql://127.0.0.1:3306/?user=root";
+                "jdbc:mysql://127.0.0.1:3306/inventorydb?useSSL=false&serverTimezone=UTC";
         private static final String USER = "root";
         private static final String PASS = "Khalidneymar11";
 
